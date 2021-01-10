b28ded171d268ed9d28a0a29f941d7d7718e09cd
VGStudios <elementswaer@gmail.com>
2021-01-09 21:51:58 -0800

    added text animation

--- app/src/main/java/com/example/nwhacks_2021/Checklist.java
+++ app/src/main/java/com/example/nwhacks_2021/Checklist.java
@@ -1,7 +1,9 @@
 package com.example.nwhacks_2021;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +16,8 @@ public class Checklist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
+        TextView header1 = (TextView) findViewById(R.id.textView4);
+        header1.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textfadein));
         String[] array = getResources().getStringArray(R.array.checklist);
         String item;
         List<String> itemList = new ArrayList<String>();
@@ -24,6 +28,7 @@ public class Checklist extends AppCompatActivity {
             item = array[new Random().nextInt(array.length)];
         }
         c1.setText(item);
+        c1.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textfadein));
 
         CheckBox c2 = (CheckBox) findViewById(R.id.checkBox2);
         item = array[new Random().nextInt(array.length)];
@@ -31,6 +36,7 @@ public class Checklist extends AppCompatActivity {
             item = array[new Random().nextInt(array.length)];
         }
         c2.setText(item);
+        c2.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textfadein));
 
         CheckBox c3 = (CheckBox) findViewById(R.id.checkBox3);
         item = array[new Random().nextInt(array.length)];
@@ -38,6 +44,7 @@ public class Checklist extends AppCompatActivity {
             item = array[new Random().nextInt(array.length)];
         }
         c3.setText(item);
+        c3.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textfadein));
 
         CheckBox c4 = (CheckBox) findViewById(R.id.checkBox4);
         item = array[new Random().nextInt(array.length)];
@@ -45,6 +52,7 @@ public class Checklist extends AppCompatActivity {
             item = array[new Random().nextInt(array.length)];
         }
         c4.setText(item);
+        c4.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textfadein));
 
     }
 }
