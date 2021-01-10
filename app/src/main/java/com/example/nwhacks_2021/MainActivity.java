be4efe9a8659b824e7e74192fd5df40630f188d7
VGStudios <elementswaer@gmail.com>
2021-01-09 18:01:04 -0800

    added other two spinners

--- app/src/main/java/com/example/nwhacks_2021/MainActivity.java
+++ app/src/main/java/com/example/nwhacks_2021/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -30,6 +32,52 @@ public class MainActivity extends AppCompatActivity {
                 spinner1[0] = mySpinner.getSelectedItem().toString();
                 TextView view = (TextView) findViewById(R.id.textView);
                 view.setText(spinner1[0]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+        Spinner mySpinner2 = (Spinner) findViewById(R.id.spinner2);
+        final String[] spinner2 = new String[1];
+
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.symptoms));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner2.setAdapter(myAdapter);
+        mySpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                parentView.getItemAtPosition(position);
+                spinner2[0] = mySpinner2.getSelectedItem().toString();
+                TextView view2 = (TextView) findViewById(R.id.textView2);
+                view2.setText(spinner2[0]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+        Spinner mySpinner3 = (Spinner) findViewById(R.id.spinner3);
+        final String[] spinner3 = new String[1];
+
+        ArrayAdapter<String> myAdapter3 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.symptoms));
+        myAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner3.setAdapter(myAdapter);
+        mySpinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                parentView.getItemAtPosition(position);
+                spinner3[0] = mySpinner3.getSelectedItem().toString();
+                TextView view3 = (TextView) findViewById(R.id.textView3);
+                view3.setText(spinner3[0]);
             }
 
             @Override
