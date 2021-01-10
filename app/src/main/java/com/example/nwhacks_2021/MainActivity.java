dbce04c70755016fc13dc24e8231939ad0bdbcbd
davidc2199 <43076641+davidc2199@users.noreply.github.com>
2021-01-09 17:52:20 -0800

    added functionality for getting dropdown list user response

--- app/src/main/java/com/example/nwhacks_2021/MainActivity.java
+++ app/src/main/java/com/example/nwhacks_2021/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.nwhacks_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        final String[] spinner1 = new String[1];
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.symptoms));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                parentView.getItemAtPosition(position);
+                spinner1[0] = mySpinner.getSelectedItem().toString();
+                TextView view = (TextView) findViewById(R.id.textView);
+                view.setText(spinner1[0]);
+            }
 
-        String selectSymptom = mySpinner.getSelectedItem().toString();
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
 
-        TextView view = (TextView) findViewById(R.id.textView);
-        view.setText(selectSymptom);
+        });
 
     }
 }
