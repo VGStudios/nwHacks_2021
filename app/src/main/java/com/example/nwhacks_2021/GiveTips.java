c7a6e7fdbf42bd737d1d2a2c6e12a1b6cf064155
alexc <alexchoi357@hotmail.co.uk>
2021-01-09 18:36:08 -0800

    added two classes and one xml for tips

---
+++ app/src/main/java/com/example/nwhacks_2021/GiveTips.java
@@ -0,0 +1,39 @@
+package com.example.nwhacks_2021;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GiveTips extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<String> list;
+    private RecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+        recyclerView = findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        list = Arrays.asList(getResources().getStringArray(R.array.android_versions));
+        adapter = new RecyclerAdapter(list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+    }
+
+}
